003e5c9a09afd7e3836a5469944cc6b46c7d864f
Natalia Tseitlina <tseytlina.ne@gmail.com>
2024-08-01 17:50:32 +0300

    feature: specific rem mode

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,4 +1,17 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Inject,
+  OnDestroy,
+  OnInit,
+  Renderer2,
+  ViewEncapsulation,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ClientUiStateHandlerService } from '@bimeister/pupakit.common';
+import { DOCUMENT } from '@angular/common';
+
+const SPECIFIC_REM_MODE_CSS_CLASS: string = 'specific-rem-mode';
 
 @Component({
   selector: 'demo-root',
@@ -7,4 +20,30 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.Emulated,
 })
-export class AppComponent {}
+export class AppComponent implements OnInit, OnDestroy {
+  private readonly subscription: Subscription = new Subscription();
+
+  constructor(
+    private readonly clientUiStateHandlerService: ClientUiStateHandlerService,
+    private readonly renderer: Renderer2,
+    @Inject(DOCUMENT) private readonly document: Document
+  ) {}
+
+  public ngOnInit(): void {
+    this.subscription.add(this.processIsSpecificRemMode());
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  private processIsSpecificRemMode(): Subscription {
+    return this.clientUiStateHandlerService.isSpecificRemMode$.subscribe((isSpecificRemMode: boolean) => {
+      if (isSpecificRemMode) {
+        this.renderer.addClass(this.document.documentElement, SPECIFIC_REM_MODE_CSS_CLASS);
+      } else {
+        this.renderer.removeClass(this.document.documentElement, SPECIFIC_REM_MODE_CSS_CLASS);
+      }
+    });
+  }
+}
